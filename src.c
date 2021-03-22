fb28a664ec2ce7d8416df285baf3d011abd818ff
Mimori256 <mimorimori256@gmail.com>
2021-03-22 15:20:00 +0900

    fix a bug

--- src.c
+++ src.c
@@ -4,41 +4,45 @@
 
 int main(void){
 
-  char str[256];
-  char cnp[] = "-=";
-  char *name;
-  char *mname;
+    char str[256];
+    char cnp[]="-=";
+    char *tmp;
+    char *name;
+    char *mname;
 
-  //define the color
-  char time_color[] = "\033[32m";
-  char artist_color[] = "\033[34m";
-  char title_color[] = "\033[34m";
+    char time_color[] = "\033[32m";
+    char artist_color[] = "\033[34m";
+    char title_color[] = "\033[34m";
 
-  int  slen;
+    int  len;
 
-  time_t now;
-  struct tm *ntime;
+    float cnt=0;
 
-  while(1){
-    fgets(str, sizeof(str), stdin);
+    time_t now;
+    struct tm *ntime;
 
-    if(str[0] == 'I'){
+    while(1){
+    fgets(str,sizeof(str),stdin);
 
-      name = strtok(NULL,"-");          
-      mname = strtok(NULL,";");         
-      mname += 1;                       
-      slen = strlen(mname);           
-      mname[slen-1] = '\0';           
+            if(str[0]=='I'){
 
-      printf("\n");
+        tmp=strtok(str,"'");          
+        name=strtok(NULL,"-");          
+        mname=strtok(NULL,";");         
+        mname+=1;                       
+        len=strlen(mname);           
+        mname[len-1]='\0';           
 
-      time(&now);
-      ntime = localtime(&now);
+        printf("\n");
 
-      printf("%s%2d/%2d  %02d:%02d\033[0m\n", time_color, ntime->tm_mon+1, ntime->tm_mday, ntime->tm_hour, ntime->tm_min);
+        time(&now);
+        ntime = localtime(&now);
 
-      printf("%sARTIST- \033[0m%s\n", artist_color,name);
-      printf("%sMUSIC-- \033[0m%s\n", title_color,mname);
+        printf("%s%2d/%2d  %02d:%02d\033[0m\n",time_color,ntime->tm_mon+1,ntime->tm_mday,ntime->tm_hour,ntime->tm_min);
+
+        printf("%sARTIST- \033[0m%s\n",artist_color,name);
+        printf("%sMUSIC-- \033[0m%s\n",title_color,mname);
+        cnt=0;
         } 
     }
 }
